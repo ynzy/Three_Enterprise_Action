a34404b142938b5dc88e813d3a652f518bcc9735
zhangyong <758922096@qq.com>
2025-03-31 21:35:42 +0800

    feat: 05-详解PBR材质纹理

---
+++ 05-详解PBR材质纹理/08-纹理加载进度情况/main.js
@@ -0,0 +1,198 @@
+import * as THREE from "three";
+// 导入轨道控制器
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+// 目标: 08-纹理加载进度情况
+
+// 创建场景
+const scene = new THREE.Scene();
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+camera.position.set(0, 0, 10);
+// 将相机添加到场景中
+scene.add(camera);
+
+var div = document.createElement("div");
+div.style.width="200px"
+div.style.height="100px"
+div.style.position = "fixed";
+div.style.top = "0px";
+div.style.left = "0px";
+div.style.color = "white";
+document.body.appendChild(div);
+
+// 设置加载管理器
+const loadingManager = new THREE.LoadingManager(
+  () => {
+    console.log("所有纹理加载完成了");
+  },
+  (url, itemsLoaded, itemsTotal) => {
+    console.log("纹理加载中...", url, itemsLoaded, itemsTotal);
+    // 计算加载进度
+    const progress = (itemsLoaded / itemsTotal) * 100;
+    let value = `${progress.toFixed(2)}%`
+    console.log(`加载进度: ${value}`);
+    // div.appendChild(document.createTextNode(`加载进度: ${value}`));
+    div.innerHTML = `加载进度: ${value}`;
+  },
+  (url) => {
+    console.log("纹理加载失败了", url);
+  }
+);
+
+
+
+// 创建材质加载器
+const textureLoader = new THREE.TextureLoader(loadingManager);
+// 导入纹理
+const doorTexture = textureLoader.load("../../assets/textures/door/color.jpg",
+  // (texture) => {
+  //   console.log("纹理加载完成了",texture);
+  // },
+  // (progress) => {
+  //   console.log("纹理加载中...",progress);
+  // },
+  // (error) => {
+  //   console.log("纹理加载失败了",error);
+  // }
+);
+
+
+// 导入透明贴图
+const doorAlphaTexture = textureLoader.load("../../assets/textures/door/alpha.jpg");
+// 导入环境遮挡贴图
+const doorAoTexture = textureLoader.load("../../assets/textures/door/ambientOcclusion.jpg");
+// 导入置换贴图
+const doorHeightTexture = textureLoader.load("../../assets/textures/door/height.jpg");
+// 导入粗糙度贴图
+const doorRoughnessTexture = textureLoader.load("../../assets/textures/door/roughness.jpg");
+// 导入金属贴图
+const doorMetalnessTexture = textureLoader.load("../../assets/textures/door/metalness.jpg");
+// 导入法线贴图
+const doorNormalTexture = textureLoader.load("../../assets/textures/door/normal.jpg");
+
+// 创建几何体
+const geometry = new THREE.BoxGeometry(1, 1, 1, 100, 100, 100);
+// 创建标准网格材质
+const material = new THREE.MeshStandardMaterial({ 
+  color: "#ffff00",
+  // 设置纹理
+  map: doorTexture,
+  // 设置透明材质
+  alphaMap: doorAlphaTexture,
+  // 设置透明
+  transparent: true,
+  // 设置环境遮挡贴图
+  aoMap: doorAoTexture,
+  // 设置环境遮挡贴图强度
+  aoMapIntensity: 1,
+  // 设置置换贴图
+  displacementMap: doorHeightTexture,
+  // 设置置换贴图
+  displacementMap: doorHeightTexture,
+  // 设置置换贴图强度
+  displacementScale: 0.1,
+  // 设置粗糙度
+  roughness: 1,
+  // 设置粗糙度贴图
+  roughnessMap: doorRoughnessTexture,
+  // 设置金属度
+  metalness: 1,
+  // 设置金属贴图
+  metalnessMap: doorMetalnessTexture,
+  // 设置法线贴图
+  normalMap: doorNormalTexture,
+ });
+//给geometry设置第二组UV
+geometry.setAttribute("uv2", new THREE.BufferAttribute(geometry.attributes.uv.array, 2)); 
+
+const mesh = new THREE.Mesh(geometry, material);
+// 将几何体添加到场景中
+scene.add(mesh);
+
+// 添加一个平面
+const planeGeometry = new THREE.PlaneGeometry(1, 1,200,200);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  // color: "#20d400",
+  map: doorTexture,
+  // 设置透明材质
+  alphaMap: doorAlphaTexture,
+  // 设置透明
+  transparent: true,
+  // 设置双面显示
+  side: THREE.DoubleSide,
+  // 设置透明度
+  // opacity: 0.5,
+  // 设置环境遮挡贴图
+  aoMap: doorAoTexture,
+  // 设置环境遮挡贴图强度
+  aoMapIntensity: 1,
+  // 设置置换贴图
+  displacementMap: doorHeightTexture,
+  // 设置置换贴图强度
+  displacementScale: 0.1,
+ });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.set(1, 0, 0);
+scene.add(plane);
+
+// 给平面设置第二组UV
+planeGeometry.setAttribute("uv2", new THREE.BufferAttribute(planeGeometry.attributes.uv.array, 2));
+
+
+// 添加环境光
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+// 添加平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+// 将webgl渲染的canvas内容添加到body
+document.body.appendChild(renderer.domElement);
+
+// 使用渲染器，通过相机将场景渲染进来
+renderer.render(scene, camera);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼，让控制器更有真实效果，必须在动画循环里调用.update()。
+controls.enableDamping = true;
+
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 循环渲染
+function render() {
+  controls.update();
+  renderer.render(scene, camera);
+  // 渲染下一帧的时候就会调用render函数
+  requestAnimationFrame(render);
+}
+render();
+
+
+// 监听窗口变化,更新渲染器渲染的尺寸
+window.addEventListener("resize", () => {
+  // 更新渲染器大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // 更新相机的长宽比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+
+});
---
+++ 05-详解PBR材质纹理/09-详解环境贴图/main.js
@@ -0,0 +1,132 @@
+import * as THREE from "three";
+// 导入轨道控制器
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+// 目标: 08-纹理加载进度情况
+
+// 创建场景
+const scene = new THREE.Scene();
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+camera.position.set(0, 0, 10);
+// 将相机添加到场景中
+scene.add(camera);
+
+var div = document.createElement("div");
+div.style.width="200px"
+div.style.height="100px"
+div.style.position = "fixed";
+div.style.top = "0px";
+div.style.left = "0px";
+div.style.color = "white";
+document.body.appendChild(div);
+
+// 设置加载管理器
+const loadingManager = new THREE.LoadingManager(
+  () => {
+    console.log("所有纹理加载完成了");
+  },
+  (url, itemsLoaded, itemsTotal) => {
+    console.log("纹理加载中...", url, itemsLoaded, itemsTotal);
+    // 计算加载进度
+    const progress = (itemsLoaded / itemsTotal) * 100;
+    let value = `${progress.toFixed(2)}%`
+    console.log(`加载进度: ${value}`);
+    // div.appendChild(document.createTextNode(`加载进度: ${value}`));
+    div.innerHTML = `加载进度: ${value}`;
+  },
+  (url) => {
+    console.log("纹理加载失败了", url);
+  }
+);
+
+const envMapName = {
+  "街道": 0,
+  "草原": 1,
+  "街区": 2,
+  "商场": 3,
+}
+const type = envMapName["草原"];
+
+// 设置cube纹理加载器
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager);
+const envMapTexture = cubeTextureLoader.load([
+  `../../assets/textures/environmentMaps/${type}/px.jpg`,
+  `../../assets/textures/environmentMaps/${type}/nx.jpg`,
+  `../../assets/textures/environmentMaps/${type}/py.jpg`,
+  `../../assets/textures/environmentMaps/${type}/ny.jpg`,
+  `../../assets/textures/environmentMaps/${type}/pz.jpg`,
+  `../../assets/textures/environmentMaps/${type}/nz.jpg`, 
+])
+
+// 创建一个球体
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+// 创建标准网格材质
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.7,
+  roughness: 0.1,
+  envMap: envMapTexture,
+});
+// 创建一个球
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// 将球体添加到场景中
+scene.add(sphere);
+
+
+
+// 添加环境光
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+// 添加平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+// 将webgl渲染的canvas内容添加到body
+document.body.appendChild(renderer.domElement);
+
+// 使用渲染器，通过相机将场景渲染进来
+renderer.render(scene, camera);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼，让控制器更有真实效果，必须在动画循环里调用.update()。
+controls.enableDamping = true;
+
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 循环渲染
+function render() {
+  controls.update();
+  renderer.render(scene, camera);
+  // 渲染下一帧的时候就会调用render函数
+  requestAnimationFrame(render);
+}
+render();
+
+
+// 监听窗口变化,更新渲染器渲染的尺寸
+window.addEventListener("resize", () => {
+  // 更新渲染器大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // 更新相机的长宽比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+
+});

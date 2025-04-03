ee09f4aa07b779f480f7b38c0844d25a291582bc
zhangyong <758922096@qq.com>
2025-04-03 14:47:05 +0800

    feat: 灯光阴影

---
+++ 06-详解灯光与阴影/03-详解聚光灯各种属性与应用/main.js
@@ -0,0 +1,118 @@
+import * as THREE from "three";
+// 导入轨道控制器
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+
+const gui = new dat.GUI();
+
+// 目标: 02-平行关阴影属性与阴影相机原理
+
+
+// 创建场景
+const scene = new THREE.Scene();
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+camera.position.set(0, 0, 10);
+// 将相机添加到场景中
+scene.add(camera);
+
+
+// 添加一个球体
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial();
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// 物体开启投射阴影
+sphere.castShadow = true;
+scene.add(sphere);
+
+// 添加一个平面
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.MeshStandardMaterial();
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.set(0, -1, 0);
+plane.rotation.x = -Math.PI / 2;
+// 物体开启接收阴影 
+plane.receiveShadow = true;
+scene.add(plane);
+
+
+
+// 添加环境光
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
+scene.add(ambientLight);
+// 添加平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(10, 10, 10);
+// 开启阴影计算
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+// 设置阴影贴图模糊度
+directionalLight.shadow.radius = 20;
+// 设置阴影贴图的分辨率
+directionalLight.shadow.mapSize.set(4096,4096);
+// 设置平行光投射相机的属性
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 500;
+directionalLight.shadow.camera.top = 5;
+directionalLight.shadow.camera.bottom = -5;
+directionalLight.shadow.camera.left = -5;
+directionalLight.shadow.camera.right = 5;
+
+
+gui.add(directionalLight.shadow.camera,"near").min(0).max(20).step(0.1).onChange(()=>{
+  directionalLight.shadow.camera.updateProjectionMatrix();
+})
+
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 设置渲染的尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+// 开启场景中阴影贴图
+renderer.shadowMap.enabled = true;
+// 将webgl渲染的canvas内容添加到body
+document.body.appendChild(renderer.domElement);
+
+// 使用渲染器，通过相机将场景渲染进来
+renderer.render(scene, camera);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼，让控制器更有真实效果，必须在动画循环里调用.update()。
+controls.enableDamping = true;
+
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 循环渲染
+function render() {
+  controls.update();
+  renderer.render(scene, camera);
+  // 渲染下一帧的时候就会调用render函数
+  requestAnimationFrame(render);
+}
+render();
+
+
+// 监听窗口变化,更新渲染器渲染的尺寸
+window.addEventListener("resize", () => {
+  // 更新渲染器大小
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // 更新相机的长宽比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+
+});
